d8d8a3cf9b172b4f02d8e0bd47402489f05f0777
Pino <hgy44@qq.com>
2019-05-17 15:14:26 +0800

    Merge branch 'master' of /Users/pino/Library/Mobile Documents/com~apple~CloudDocs/Documents/IDEA/Database-System-Project with conflicts.

--- After_End/src/view/StartMenu.java
+++ After_End/src/view/StartMenu.java
@@ -1,104 +1,40 @@
 package view;
 
+import dao.BaseDao;
+import java.sql.*;
 import java.util.*;
 
-public class StartMenu extends View{
-     private int inputID;
-     private String inputPassword;
-     private boolean isAdmin=false;
-     private View view;
-
+public class StartMenu {
+    static Scanner in = new Scanner(System.in);
     public static void main(String[] args) {
-        StartMenu startMenu = new StartMenu();
-        startMenu.displayMenu();
+        System.out.println("run");
+        BaseDao temp = new BaseDao();
+        try{
+            temp.getCon();
+        }catch (Exception e){
+            e.printStackTrace();
+        }finally {
+            temp.closeAll();
+        }
+
 
     }
 
     public void loginMenu()
     {
         System.out.println("UserID: ");
-        inputID = Integer.parseInt(in.nextLine());
+        String ID = in.next();
         System.out.println("Password: ");
-        inputPassword = in.nextLine();
-
-        if(!checkPassword())//密码不正确
-        {
-            System.out.println("密码错误！");
-            return;
-        }
-
-        //登陆成功
-        if(isAdmin)
-            view = new AdminView(inputID,inputPassword);
-        else
-            view = new UserView(inputID,inputPassword);
-
-        view.displayMenu();
+        String password = in.next();
 
     }
 
     public void display()
     {
-        System.out.println("欢迎使用社团管理系统\n"
-            + "-----------------\n"
-            + "1.登陆\n"
-            + "2.登陆(管理员)\n"
-            + "3.注册\n"
-            + "4.退出\n");
+        System.out.println("1.login\n"
+                +"2.login(admin)\n"
+                +"3.sign up"
+                +"4.exit");
     }
-
-
-    public void displayMenu()
-    {
-        final int EXIT = 4;
-        int instruction = -1;
-        while (true)
-        {
-            display();
-            System.out.print('>');
-            String temp = in.nextLine();
-            if(temp.length() == 1)
-            {
-                if(Character.isDigit(temp.charAt(0)))
-                    instruction = Character.getNumericValue(temp.charAt(0));
-            }
-
-            if(instruction == EXIT)
-                break;
-
-            switch (instruction) {
-                case 1:
-                    isAdmin = false;
-                    loginMenu();
-                    break;
-                case 2:
-                    isAdmin = true;
-                    loginMenu();
-                    break;
-                case 3:
-                    //todo 注册
-                    break;
-                default:
-                    System.out.println("-------------\n"
-                                      +"invalid input\n"
-                                      +"-------------\n");
-            }
-        }
-    }
-
-    public boolean checkPassword()
-    {
-        boolean result = true;
-        if(isAdmin)
-        {
-            //todo Admin Password check
-        }
-        else
-        {
-            //todo User Password check
-        }
-        return result;
-    }
-
 
 }
